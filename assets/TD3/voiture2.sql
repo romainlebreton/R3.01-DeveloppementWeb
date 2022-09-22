a5fbd3166410872880b2d501b1d9433e5a2a5c38
Romain Lebreton <romainlebreton@gmail.com>
2022-09-22 17:12:44 +0200

    Màj table voiture2 avec une colonne nbSieges

--- assets/TD3/voiture2.sql
+++ assets/TD3/voiture2.sql
@@ -1,13 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Dim 18 Septembre 2016 à 19:40
--- Version du serveur :  5.7.15-0ubuntu0.16.04.1
--- Version de PHP :  7.0.8-0ubuntu0.16.04.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,34 +9,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `PHP1617`
---
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `voiture2`
 --
 
 CREATE TABLE `voiture2` (
   `immatriculation` varchar(8) NOT NULL,
   `marque` varchar(25) NOT NULL,
-  `couleur` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `couleur` varchar(12) NOT NULL,
+  `nbSieges` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Contenu de la table `voiture2`
+-- Déchargement des données de la table `voiture2`
 --
 
-INSERT INTO `voiture2` (`immatriculation`, `marque`, `couleur`) VALUES
-('12345678', 'Ferrari', 'vert'),
-('259641', 'Renault', 'Noire'),
-('710097', 'Tesla', 'Bleu nuit'),
-('8369210', 'Tesla', 'Bleu nuit'),
-('abc124az', 'Renault', 'bleu');
+INSERT INTO `voiture2` (`immatriculation`, `marque`, `couleur`, `nbSieges`) VALUES
+('12345678', 'Ferrari', 'vert', 2),
+('259641', 'Renault', 'Noire', 5),
+('710097', 'Tesla', 'Bleu nuit', 4),
+('8369210', 'Tesla', 'Bleu nuit', 4),
+('abc124az', 'Renault', 'bleu', 5);
 
 --
--- Index pour les tables exportées
+-- Index pour les tables déchargées
 --
 
 --
@@ -52,6 +39,7 @@ INSERT INTO `voiture2` (`immatriculation`, `marque`, `couleur`) VALUES
 --
 ALTER TABLE `voiture2`
   ADD PRIMARY KEY (`immatriculation`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
